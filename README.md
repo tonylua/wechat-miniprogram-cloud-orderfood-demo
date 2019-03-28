51f7d00d4db0b6e1bdc360a0a3556fd4bcefbf6f
tonylua <tonylua@sina.com>
2019-03-28 23:00:09 +0800

    add code.png

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wechat-miniprogram-cloud-orderfood-demo
 微信小程序云开发 demo 组队点餐
+
+![](./code.png)
--- src/miniprogram/app.js
+++ src/miniprogram/app.js
@@ -6,7 +6,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        env: 'prod-407cc5',
+        // env: 'prod-407cc5',
         traceUser: true,
       })
     }
